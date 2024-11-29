d45663fc0b4208dc5efc28ba6f8b49872a048a45
Vladimir Tomic <vladimir.tomic@financescout24.ch>
2024-11-29 12:59:19 +0100

    feat: fixed version

--- app/page.tsx
+++ app/page.tsx
@@ -2,100 +2,50 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <main style={{ padding: "24px" }}>
+      <div style={{ fontFamily: "fantasy", fontSize: "20px", color: "red" }}>
+        <p>Hi Katarina,</p>
+        <p>
+          Wow, you've made some serious progress in solving these puzzles! I'm
+          proud of you!
+        </p>
+        <p>
+          You're very close to your{" "}
+          <span style={{ fontWeight: "bold", fontSize: "24px" }}>gift</span>,
+          don't give up now...
+        </p>
+        <p>
+          Do you remember that{" "}
+          <span style={{ fontWeight: "bold", fontSize: "24px" }}>e-mail</span>{" "}
+          that you received? Hopefully you didn't delete it... It contains{" "}
+          <span style={{ fontWeight: "bold", fontSize: "24px" }}>
+            four numbers
+          </span>
+          . When combined, they form the code for the{" "}
+          <span style={{ fontWeight: "bold", fontSize: "24px" }}>locker</span>.
+        </p>
+        <p>
+          The locker is at the{" "}
+          <span style={{ fontWeight: "bold", fontSize: "24px" }}>
+            2nd floor
+          </span>
+          . I didn't want to be so mean to make you go all the way to the 8th
+          floor.
+        </p>
+        <p>
+          But which locker exactly is it? Well... It has to do something with
+          your{" "}
+          <span style={{ fontWeight: "bold", fontSize: "24px" }}>
+            birth date
+          </span>
+          ...
+        </p>
+        <p>Now go inspect all lockers and try to find out which one it is.</p>
+        <p>Almost there... Good luck!</p>
+        <p>Ho ho ho!</p>
+        <br />
+        <Image src="/santa.png" alt="Santa" width={180} height={180} />
+      </div>
+    </main>
   );
 }
